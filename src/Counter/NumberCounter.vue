8d5e0beb196e5855904d35077bcd92d4c9affe63
Adam Christopher <adam.keristoper@gmail.com>
2018-07-08 10:27:56 +0900

    Have yourself a counter

---
+++ src/Counter/NumberCounter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button type="button" @click="decreaseNumber()">-</button>
+    <span>{{ number }}</span>
+    <button type="button" @click="increaseNumber()">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    number: Number
+  },
+  methods: {
+    increaseNumber () {
+      this.$emit('increaseNumber')
+    },
+    decreaseNumber () {
+      this.$emit('decreaseNumber')
+    }
+  }
+}
+</script>
--- src/Counter/index.vue
+++ src/Counter/index.vue
@@ -1,16 +1,34 @@
 <template>
-  <p>{{ title }}</p>
+  <div>
+    <p>{{ title }}</p>
+    <number-counter
+      :number="number"
+      @increaseNumber="increaseNumber"
+      @decreaseNumber="decreaseNumber"
+    />
+    <p>Your number is: {{ number }}</p>
+  </div>
 </template>
 
 <script>
+import NumberCounter from './NumberCounter.vue'
 export default {
+  components: {
+    NumberCounter
+  },
   data () {
     return {
-      title: 'bananas'
+      title: 'bananas',
+      number: 0
     }
   },
-  created () {
-    console.log('haha bananas')
+  methods: {
+    increaseNumber () {
+      this.number += 1
+    },
+    decreaseNumber () {
+      this.number -= 1
+    }
   }
 }
 </script>
